9361ee9c9ab0ac401ab94ec0b63fae19e13aee70
Sergio A. B. Petrovcic <79282690+sergiopetrovcic@users.noreply.github.com>
2023-09-12 17:43:54 -0300

    Adicionado LineRenderer

--- Aula - Raycast/Assets/RaycastExample.cs
+++ Aula - Raycast/Assets/RaycastExample.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class RaycastExample : MonoBehaviour
 {
     int mask;
+    private LineRenderer _lineRenderer;
 
     private void Start()
     {
         mask = LayerMask.GetMask("Sphere");
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.SetPosition(1, transform.forward);
     }
 
     void Update()
@@ -18,12 +21,17 @@ public class RaycastExample : MonoBehaviour
     {
         RaycastHit hit;
 
-        //Ray ray = new Ray(transform.position, transform.forward);
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = new Ray(transform.position, transform.forward);
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit, 50, ~mask))
         {
             print("Acertou algo a " + hit.distance + " metros.");
             Debug.DrawLine(ray.origin, hit.point);
+            _lineRenderer.SetPosition(1, hit.point);
+        }
+        else
+        {
+            _lineRenderer.SetPosition(1, transform.forward);
         }
 
         Debug.DrawRay(ray.origin, ray.direction, Color.green);
